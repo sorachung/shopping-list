45bcb762a308867c835df9a08992745bb491450a
sorachung <sorachung.src@gmail.com>
2021-11-18 14:50:00 -0600

    cleaned up code

--- shopping.js
+++ shopping.js
@@ -1,69 +1,64 @@
 const shoppingList = [
-    {
-        id: 1,
-        item: "milk",
-        price: "2.99"
-    },
-    {
-        id: 2,
-        item: "eggs",
-        price: "1.99"
-    },
-    {
-        id: 3,
-        item: "pizza rolls",
-        price: "10.99"
-    },
-    {
-        id: 4,
-        item: "apples",
-        price: "4.99"
-    },
-    {
-        id: 5,
-        item: "sandwich buns",
-        price: "9.99"
-    }
-]
+  {
+    id: 1,
+    item: "milk",
+    price: "2.99",
+  },
+  {
+    id: 2,
+    item: "eggs",
+    price: "1.99",
+  },
+  {
+    id: 3,
+    item: "pizza rolls",
+    price: "10.99",
+  },
+  {
+    id: 4,
+    item: "apples",
+    price: "4.99",
+  },
+  {
+    id: 5,
+    item: "sandwich buns",
+    price: "9.99",
+  },
+];
 
-// console.log(shoppingList);
-
-shoppingList.forEach((item) => item.dateCreated = new Date());
-
-
+shoppingList.forEach((item) => (item.dateCreated = new Date()));
 
 const bananas = {
-    item: "bananas",
-    price: 1.49
-}
+  item: "bananas",
+  price: 1.49,
+};
 
-addToShoppingList(bananas)
+addToShoppingList(bananas);
 
-console.log(shoppingList)
-console.log("items over $8:", filterExpensiveItems())
+console.log(shoppingList);
+console.log("items over $8:", filterExpensiveItems());
 
 //functions
 
 function addToShoppingList(shopItem) {
-    // get id
-    const idsArray = shoppingList.map(item => item.id);
-    const id = Math.max(...idsArray) + 1;
-    shopItem.id = id;
-    const today = new Date()
-    // const date = `${today.getMonth()+1}/${today.getDay()}/${today.getFullYear()} ${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`
-               
-    shopItem.dateCreated = today;
-    
-    shoppingList.push(shopItem);
+  // get id
+  const idsArray = shoppingList.map((item) => item.id);
+  const id = Math.max(...idsArray) + 1;
+  shopItem.id = id;
+  const today = new Date();
+  // const date = `${today.getMonth()+1}/${today.getDay()}/${today.getFullYear()} ${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`
+
+  shopItem.dateCreated = today;
+
+  shoppingList.push(shopItem);
 }
 
 function filterExpensiveItems() {
-    const expensiveItems = [];
-    for (const item of shoppingList) {
-        if (item.price >= 8) {
-            expensiveItems.push(item)
-        }
+  const expensiveItems = [];
+  for (const item of shoppingList) {
+    if (item.price >= 8) {
+      expensiveItems.push(item);
     }
-    return expensiveItems;
+  }
+  return expensiveItems;
 }
-
